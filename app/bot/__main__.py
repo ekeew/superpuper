7f837123325f5ed3c008b00e7095d634fb8e1fc5
toster <dsodx@pm.me>
2023-07-08 19:21:27 +0300

    Add main

---
+++ app/bot/__main__.py
@@ -0,0 +1,33 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+
+from .config import Settings
+
+
+async def main() -> None:
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(levelname)-8s [%(asctime)s] :: %(name)s : %(message)s"
+    )
+    logger = logging.getLogger(__name__)
+
+    config = Settings()
+
+    bot = Bot(token=config.bot.token.get_secret_value())
+    dp = Dispatcher()
+
+    try:
+        logger.warning("Running bot")
+        await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+    finally:
+        await dp.storage.close()
+        logger.warning("Bot stopped")
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
