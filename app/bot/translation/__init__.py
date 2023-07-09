f0ced3f8a0d356b8eecc5024adf8c808bf15aced
toster <dsodx@pm.me>
2023-07-09 12:42:41 +0300

    Fix locales

--- app/bot/translation/__init__.py
+++ app/bot/translation/__init__.py
@@ -4,17 +4,13 @@ from fluent_compiler.bundle import FluentBundle
 from fluentogram import TranslatorHub, FluentTranslator
 
 
-def _get_ftl_files(locale_dir: Path) -> list[Path]:
-    return [file for file in locale_dir.iterdir() if file.suffix == ".ftl"]
-
-
 def get_translator_hub() -> TranslatorHub:
-    locales_dir = Path(__file__) / "locales"
+    locales_dir = Path(__file__).parent / "locales"
     translation_hub = TranslatorHub(
         {"ru": "ru", "en": "en"},
         [
-            FluentTranslator("ru", FluentBundle("ru-RU", _get_ftl_files(locales_dir / "ru"))),
-            FluentTranslator("en", FluentBundle("en-US", _get_ftl_files(locales_dir / "en"))),
+            FluentTranslator("ru", FluentBundle("ru-RU", [locales_dir / "ru" / "main.ftl"])),
+            FluentTranslator("en", FluentBundle("en-US", [locales_dir / "en" / "main.ftl"])),
         ]
     )
     return translation_hub
