b00151d402ca7fef7f798b0bc43db89dfdcab0d6
toster <dsodx@pm.me>
2023-07-09 12:22:55 +0300

    Add db

---
+++ app/bot/middlewares/db.py
@@ -0,0 +1,22 @@
+from typing import Callable, Dict, Any, Awaitable
+
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+from sqlalchemy.ext.asyncio import async_sessionmaker
+
+from app.core.db import DbAdapter
+
+
+class DbMiddleware(BaseMiddleware):
+    def __init__(self, session_pool: async_sessionmaker) -> None:
+        self.session_pool = session_pool
+
+    async def __call__(
+            self,
+            handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+            event: TelegramObject,
+            data: Dict[str, Any]
+    ) -> Any:
+        async with self.session_pool() as session:
+            data["db"] = DbAdapter(session)
+            return await handler(event, data)
