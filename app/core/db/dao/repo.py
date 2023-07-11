61454c6b1492e97cdc4f60f1711c5f36052fb366
toster <dsodx@pm.me>
2023-07-11 11:33:44 +0300

    Add worker repo

--- app/core/db/dao/repo.py
+++ app/core/db/dao/repo.py
@@ -1,12 +1,16 @@
+from nats import NATS
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .user import _UserRepo
+from .worker import _WorkerRepo
 
 
 class DbRepo:
-    def __init__(self, session: AsyncSession) -> None:
+    def __init__(self, session: AsyncSession, client: NATS) -> None:
         self.session = session
+        self.client = client
         self.user = _UserRepo(session)
+        self.worker = _WorkerRepo(client)
 
     async def commit(self) -> None:
         await self.session.commit()
---
+++ app/core/db/dao/worker.py
@@ -0,0 +1,10 @@
+from nats import NATS
+
+
+class _WorkerRepo:
+    def __init__(self, client: NATS) -> None:
+        self.client = client
+        self.stream = client.jetstream()
+
+    async def send(self, chat_id: int, text: str) -> None:
+        await self.stream.publish("send.messages", text.encode("utf-8"), headers={"chat_id": chat_id})
