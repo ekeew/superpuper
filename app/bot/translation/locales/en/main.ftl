8731f8ac06efee1ee7fc05cbb5d407362664c0d7
toster <dsodx@pm.me>
2023-07-11 11:36:50 +0300

    Update

--- app/bot/translation/locales/en/main.ftl
+++ app/bot/translation/locales/en/main.ftl
@@ -1,2 +1,4 @@
 
 hello = Hello, world!
+
+success-mail = Mail completed
--- app/core/db/dao/worker.py
+++ app/core/db/dao/worker.py
@@ -6,5 +6,5 @@ class _WorkerRepo:
         self.client = client
         self.stream = client.jetstream()
 
-    async def send(self, chat_id: int, text: str) -> None:
+    async def send_message(self, chat_id: int, text: str) -> None:
         await self.stream.publish("send.messages", text.encode("utf-8"), headers={"chat_id": chat_id})
