b289a9468e98ecc7ce5655af77d52667e66010fe
toster <dsodx@pm.me>
2023-07-11 12:04:52 +0300

    Add admin filter

--- app/bot/handlers/admin.py
+++ app/bot/handlers/admin.py
@@ -1,20 +1,22 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import Command, CommandObject
 from aiogram.types import Message
 from fluentogram import TranslatorRunner
+
+from app.core.config import Settings
 from app.core.db import DbRepo
 
 
 async def cmd_send(message: Message, command: CommandObject, db: DbRepo, i18n: TranslatorRunner) -> None:
-    for user_id in db.user.get_all_ids():
+    for user_id in await db.user.get_all_ids():
         await db.worker.send_message(user_id, command.args)
     await message.answer(i18n.get("success-mail"))
 
 
-def get_common_router() -> Router:
+def get_admin_router() -> Router:
+    config = Settings()
     router = Router()
 
-    router.message.register(cmd_send, Command("send"))
+    router.message.register(cmd_send, Command("send"), F.chat.id.in_(config.admin_ids))
 
     return router
-
