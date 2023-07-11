74c000f82b70e17eb8c3e80f6b085a2dcc3021dc
toster <dsodx@pm.me>
2023-07-11 12:37:53 +0300

    Add hello command

--- app/bot/handlers/common.py
+++ app/bot/handlers/common.py
@@ -1,14 +1,22 @@
-from aiogram import Router
-from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram import Router, Bot
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, BotCommandScopeChat
 from fluentogram import TranslatorRunner
 
 from app.core.db import DbRepo
+from ..ui import get_default_commands
 
 
-async def cmd_start(message: Message, db: DbRepo, i18n: TranslatorRunner) -> None:
+async def cmd_start(message: Message, bot: Bot, db: DbRepo, i18n: TranslatorRunner) -> None:
     if await db.user.add(message.from_user.id):
         await db.commit()
+        scope = BotCommandScopeChat(chat_id=message.chat.id)
+        await bot.set_my_commands(get_default_commands(i18n), scope)
+
+    await message.answer(i18n.get("hello"))
+
+
+async def cmd_hello(message: Message, i18n: TranslatorRunner) -> None:
     await message.answer(i18n.get("hello"))
 
 
@@ -16,5 +24,6 @@ def get_common_router() -> Router:
     router = Router()
 
     router.message.register(cmd_start, CommandStart())
+    router.message.register(cmd_hello, Command("hello"))
 
     return router
