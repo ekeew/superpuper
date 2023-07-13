deb0c213fd9a5ecd0e95c744ad44b7f15c49aae4
toster <dsodx@pm.me>
2023-07-13 20:25:28 +0300

    Update worker

--- app/core/workers/notify.py
+++ app/core/workers/notify.py
@@ -9,11 +9,6 @@ from taskiq_nats import NatsBroker
 
 from app.core.config import Settings
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(levelname)-8s [%(asctime)s] :: %(name)s : %(message)s"
-)
-
 config = Settings()
 broker = NatsBroker(config.nats.dsn, queue="notifying")
 scheduler = TaskiqScheduler(broker, [LabelScheduleSource(broker)])
@@ -21,20 +16,28 @@ scheduler = TaskiqScheduler(broker, [LabelScheduleSource(broker)])
 
 @broker.on_event(TaskiqEvents.WORKER_STARTUP)
 async def startup(state: TaskiqState) -> None:
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(levelname)-8s [%(asctime)s] :: %(name)s : %(message)s"
+    )
+    logger = logging.getLogger(__name__)
+    logger.warning("Starting scheduler...")
+
     bot = Bot(config.bot.token.get_secret_value())
 
     state.bot = bot
     state.admin_id = config.admin_id
+    state.logger = logger
 
 
 @broker.on_event(TaskiqEvents.WORKER_SHUTDOWN)
 async def shutdown(state: TaskiqState) -> None:
     await state.bot.session.close()
+    await state.logger.warning("Scheduler stopped")
 
 
 @broker.task(task_name="notify", schedule=[{"cron": "* 12 * * *"}])
 async def notify(context: Context = TaskiqDepends()) -> None:
-    print(1)
     bot: Bot = context.state.bot
     admin_id: int = context.state.admin_id
     with suppress(TelegramForbiddenError):
