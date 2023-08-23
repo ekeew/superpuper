b4387c54ee9685d940f6ef5ac6e2db09deaa5e02
dsodx <dsodx@pm.me>
2023-08-23 11:22:37 +0300

    Update alembic script

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -7,28 +7,16 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-from src.app.core.db import Base
-from src.app.core.config import Settings
+from app.core.db import Base
+from app.core.config import Settings
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 config.set_main_option("sqlalchemy.url", Settings().postgres.dsn)
 
 
