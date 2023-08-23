9cbb29934f21934bb60551e46c3a39f16415520d
dsodx <dsodx@pm.me>
2023-08-23 12:09:16 +0300

    Fix missing `@abstractmethod`

--- src/app/core/interfaces/dao/broker/mailing.py
+++ src/app/core/interfaces/dao/broker/mailing.py
@@ -1,7 +1,8 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class BaseMailing(ABC):
 
+    @abstractmethod
     async def send_message(self, chat_id: int, text: str) -> None:
         raise NotImplementedError
--- src/app/core/interfaces/dao/database/user.py
+++ src/app/core/interfaces/dao/database/user.py
@@ -1,10 +1,12 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class BaseUser(ABC):
 
+    @abstractmethod
     async def add(self, tg_id: int) -> bool:
         raise NotImplementedError
 
+    @abstractmethod
     async def get_all_ids(self) -> list[int]:
         raise NotImplementedError
