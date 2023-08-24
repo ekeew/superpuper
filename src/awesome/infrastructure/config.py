0eea5f9991cd835317c05b66577e09ad7df55216
dsodx <dsodx@pm.me>
2023-08-24 16:07:20 +0300

    Update config

--- src/awesome/infrastructure/config.py
+++ src/awesome/infrastructure/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from typing import ClassVar
 
-from pydantic import BaseModel, PostgresDsn, SecretStr
+from pydantic import BaseModel, PostgresDsn, SecretStr, Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -9,7 +9,7 @@ class Bot(BaseModel):
     token: SecretStr
 
 
-class PostgreSQL(BaseModel):
+class Postgres(BaseModel):
     dsn: PostgresDsn
 
 
@@ -17,11 +17,11 @@ class Nats(BaseModel):
     dsn: str
 
 
-class Settings(BaseSettings):
+class ApplicationSettings(BaseSettings):
     bot: Bot
-    postgres: PostgreSQL
+    postgres: Postgres
     nats: Nats
 
-    project_dir: ClassVar[Path] = Path(__file__).parent.parent.parent.parent.resolve()
+    project_dir: Path = Field(Path(__file__).parent.parent.parent.parent.resolve(), const=True)
 
     model_config = SettingsConfigDict(env_file=project_dir / ".env", env_nested_delimiter="_")
